0eaf86997baa51a286b3842a629b675bde1099db
Null <rosslwheelerjr@gmail.com>
2023-03-11 16:34:50 -0700

    Added the ability to use the names as permission nodes and still count it as that whitelist level

--- src/main/java/me/supercoolspy/levelsofwhitelist/JoinListener.java
+++ src/main/java/me/supercoolspy/levelsofwhitelist/JoinListener.java
@@ -8,7 +8,10 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.ChatColor;
 import org.bukkit.permissions.PermissionAttachmentInfo;
 
+import java.util.NoSuchElementException;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class JoinListener implements Listener {
     private final LevelsOfWhitelist plugin = LevelsOfWhitelist.getInstance();
@@ -17,23 +20,37 @@ public class JoinListener implements Listener {
         if (!plugin.isWhitelistEnabled()) return;
         System.out.println("Join event");
         final Player player = event.getPlayer();
-        final int[] level = {0};
+        final AtomicInteger level = new AtomicInteger();
         for (PermissionAttachmentInfo permission : player.getEffectivePermissions()) {
             if (permission.getPermission().startsWith("levelsofwhitelist.level.")) {
                 try {
                     int newLevel = Integer.parseInt(permission.getPermission().split("\\.")[2]);
-                    if (newLevel > level[0]) {
-                        level[0] = newLevel;
+                    if (newLevel > level.get()) {
+                        level.set(newLevel);
                     }
-                    if (level[0] >= plugin.getCurrentLevel()) {
+                    if (level.get() >= plugin.getCurrentLevel()) {
                         break;
                     }
                 } catch (NumberFormatException error) {
-                    plugin.getLogger().warning("Invalid permission: " + permission.getPermission());
+                    try {
+                        Optional<WhitelistLevel> wlLevel = LevelsOfWhitelist.getInstance().getLevels().stream().filter
+                                (whitelistLevel -> whitelistLevel.getPermIdentifier().equals(permission.getPermission().split("\\.")[2])).findFirst();
+                        if (wlLevel.isPresent()) {
+                            int newLevel = wlLevel.orElseThrow().getLevel();
+                            if (newLevel > level.get()) {
+                                level.set(newLevel);
+                            }
+                            if (level.get() >= plugin.getCurrentLevel()) {
+                                break;
+                            }
+                        }
+                    } catch (NullPointerException | NoSuchElementException e) {
+                        plugin.getLogger().warning("Invalid permission: " + permission.getPermission());
+                    }
                 }
             }
         }
-        if (level[0] < plugin.getCurrentLevel() && !player.hasPermission("levelsofwhitelist.bypass")) {
+        if (level.get() < plugin.getCurrentLevel() && !player.hasPermission("levelsofwhitelist.bypass")) {
             player.kickPlayer(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(plugin.getConfig().getString("KickMSG"))));
         } else {
             player.sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(plugin.getConfig().getString("WhitelistedWelcomeMSG"))));
